19c6eefa84e9d4debb3ee4f4d4277947621ff903
Alexey Cluster <cluster@cluster.wtf>
2026-04-05 15:21:59 +0200

    Fix TIM2 interrupt handler to ensure proper execution order by moving USER CODE END comment

--- Core/Src/stm32f1xx_it.c
+++ Core/Src/stm32f1xx_it.c
@@ -243,9 +243,8 @@ void EXTI9_5_IRQHandler(void)
 void TIM2_IRQHandler(void)
 {
   /* USER CODE BEGIN TIM2_IRQn 0 */
-
-  /* USER CODE END TIM2_IRQn 0 */
   prec_time_tick();
+  /* USER CODE END TIM2_IRQn 0 */
   HAL_TIM_IRQHandler(&htim2);
   /* USER CODE BEGIN TIM2_IRQn 1 */
 
