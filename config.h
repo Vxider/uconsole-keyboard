375ef85ed15132b667c538feecb42d5f4e24fd70
vxider <lb@vxider.com>
2026-05-17 00:41:37 +0800

    Adjust keyboard backlight timeout

--- config.h
+++ config.h
@@ -11,7 +11,7 @@ static const uint16_t backlight_vals[3] = {0, 500, 2000};
 
 // The time of inactivity in seconds after which the keyboard backlight will turn off
 // 0 = never turn off
-#define KEYBOARD_BACKLIGHT_OFF_TIME 15
+#define KEYBOARD_BACKLIGHT_OFF_TIME 30
 
 // The duration of the keyboard backlight dim out effect in milliseconds
 #define KEYBOARD_BACKLIGHT_DIM_OUT_DURATION 1000
@@ -21,7 +21,7 @@ static const uint16_t backlight_vals[3] = {0, 500, 2000};
 
 // If defined, the keyboard backlight will resume when the trackball is moved
 // 0 = resume only on keypress, 1 = resume on keypress and trackball movement
-#define KEYBOARD_BACKLIGHT_RESUME_BY_TRACKBALL 1
+#define KEYBOARD_BACKLIGHT_RESUME_BY_TRACKBALL 0
 
 // If defined, the double semicolon will be replaced with an apostrophe
 // This is useful for typing Russian text with the keyboard (жж -> э)
